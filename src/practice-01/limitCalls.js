3f9f9651212b7d942109aa0a73b0e69e84a89679
Daria <econs.dk@gmail.com>
2020-01-15 22:10:52 +0700

    added limitCalls

--- src/practice-01/limitCalls.js
+++ src/practice-01/limitCalls.js
@@ -19,4 +19,13 @@
  * @param  {number} maxCalls максимальное количество вызовов
  * @return {Function}
  */
-export function limitCalls(fn, maxCalls) { return () => {}; }
+export function limitCalls(fn, maxCalls) { 
+  let count = 0;
+  
+  return () => {
+    if (count < maxCalls) { 
+      fn();
+      count++;
+    }
+  }; 
+}
