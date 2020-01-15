7970d269481239ed37050bf44d8b1dff8a900f4d
Алексей Кунчий <gris.mego@gmail.com>
2020-01-15 22:41:11 +0700

    get min & max

--- src/practice-01/getMinMax.js
+++ src/practice-01/getMinMax.js
@@ -16,4 +16,10 @@
  * @param  {string} input входная строка
  * @return {{min: number, max: number}} объект с минимумом и максимумом
  */
-export function getMinMax(input) {return 0; }
+export function getMinMax(input) {
+    let sortedArray = input.replace(';', ' ').split(",").join(" ").split(' ').filter((isNumber) => Number(isNumber)).map((it) => Number(it)).sort((a, b) => b - a);
+    return {
+        max: sortedArray[0],
+        min: sortedArray[sortedArray.length - 1]
+    } 
+}
